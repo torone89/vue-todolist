29b9e2ea43c80e1c5dca9462eefbd1d88c7dfc08
torone89 <torone89>
2022-07-13 14:54:49 +0200

    Miolestone 3 Da fix

--- js/script.js
+++ js/script.js
@@ -25,7 +25,8 @@ const listaApp = new Vue({
       // MILESTONE 1
 // Stampare all'interno di una lista, un item per ogni todo.
 // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-      tasks: [
+newTask: '',  
+tasks: [
         {
             text: 'Lavarsi i Denti',
             done: false
@@ -42,15 +43,25 @@ const listaApp = new Vue({
             text: 'Rifare il letto',
             done: true
         }
-    ]
+    ],
+ 
   }, 
    
 
 // MILESTONE 2
 // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
 // il todo viene rimosso dalla lista.
-  methods: { removeTask (index){
+  methods: {removeTask (index){
       this.tasks.splice(index, 1)
+  },
+        // MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il 
+// testo digitato viene letto e utilizzato per creare un nuovo todo, 
+// che quindi viene aggiunto alla lista dei todo esistenti.
+  addTask(){
+this.tasks.push(this.newTask)
+// Reset Html
+this.newTask =""
   }
   },
    
