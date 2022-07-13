b85cc6cffa9a1046125a73f6702c8217eb25094a
torone89 <torone89>
2022-07-13 16:19:52 +0200

    Bonus 1 Ok

--- js/script.js
+++ js/script.js
@@ -7,13 +7,6 @@ console.log("JS")
 console.log('Vue ok', Vue)
 
 
-// MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il 
-// testo digitato viene letto e utilizzato per creare un nuovo todo, 
-// che quindi viene aggiunto alla lista dei todo esistenti.
-// Bonus:
-// 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il 
-// todo alla lista
 // 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo
 //  corrispondente (se done era uguale a false, impostare true e viceversa)
 
@@ -25,7 +18,7 @@ const listaApp = new Vue({
     // MILESTONE 1
     // Stampare all'interno di una lista, un item per ogni todo.
     // Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
-    nuovaTask: "",
+
     tasks: [
       {
         text: 'Lavarsi i Denti',
@@ -47,7 +40,6 @@ const listaApp = new Vue({
 
   },
 
-
   // MILESTONE 2
   // Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, 
   // il todo viene rimosso dalla lista.
@@ -64,7 +56,6 @@ const listaApp = new Vue({
         text: textelement
       }
       this.tasks.push(nuovaLista)
-      // Reset Html
 
     }
   },
